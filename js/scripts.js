8051eec7b82fc150e43978fb7d0ea1d4281948a1
Drew Henderson <drewhendersonemail@gmail.com>
2021-10-29 10:04:19 -0700

    Passed third test, beepBoop() replaces any number with a 1 with Beep!

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 function beepBoop(input) {
-  input = parseInt(input);
-  if (input === 0) {
-    return "0";
+  let result = "";
+  for (let i = 0; i <= parseInt(input); i++) {
+    let value = 0;
+    let digits = i.toString().split("");
+    digits.forEach(function(thing) {
+      if (thing === '1') {
+        value = 1;
+      }
+    });
+    if (value === 1) {
+      result = result.concat("Beep!, ")
+    }
+    else {
+      result = result.concat(i.toString() + ", ");
+    }
+    console.log(digits);
+    console.log(value);
+    console.log(result);
   }
-  if (input === 1) {
-    return "0, Beep!"
-  }
+  return result;
 }
 
-console.log(beepBoop("1"));
+console.log(beepBoop("122"));
