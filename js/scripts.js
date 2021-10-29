bb66eb8ce1b236865503eb9ba29901eec39b8505
Drew Henderson <drewhendersonemail@gmail.com>
2021-10-29 13:56:20 -0700

    added reverse ordering and custom name option

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
-function beepBoop(input) {
+function beepBoop(input, name, reverse) {
   let result = "";
-  for (let i = 0; i <= parseInt(input); i++) {
+  let i = 0;
+  if (name) {
+    name = (", ").concat(name);
+  }
+  for (let i2 = 0; i2 <= parseInt(input); i2++) {
+    if (reverse === 'yes') {
+      i = input - i2;
+    }
+    else {
+      i = i2;
+    }
     let choice = 0;
     let digits = i.toString().split("");
     digits.forEach(function(digit) {
@@ -11,13 +21,13 @@ function beepBoop(input) {
       }
     });
     if (choice === 1) {
-      result = result.concat("Beep!, ")
+      result = result.concat("Beep! ")
     }
     else if (choice === 2) {
-      result = result.concat("Boop!, ")
+      result = result.concat("Boop! ")
     }
     else if (choice === 3) {
-      result = result.concat("Won't you be my neighbor?, ")
+      result = result.concat("Won't you be my neighbor" + name + "? ")
     }
     else {
       result = result.concat(i.toString() + ", ");
@@ -33,7 +43,7 @@ function beepBoop(input) {
 $(document).ready(function() {
   $("#form1").submit(function(event) {
     event.preventDefault();
-    $(".results").text(beepBoop($("input#input").val()));
+    $(".results").text(beepBoop($("input#input").val(), $("input#name").val(), $("input:radio[name=reverse]:checked").val()));
     $("#results").show();
   });
 });
