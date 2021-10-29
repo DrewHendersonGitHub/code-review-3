9a861c77834b1cffb57ea0028fe9926fbb9b7a75
Drew Henderson <drewhendersonemail@gmail.com>
2021-10-29 09:11:48 -0700

    Passed second test, beepBoop(1) correctly returns 0, Beep!

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ function beepBoop(input) {
   if (input === 0) {
     return "0";
   }
+  if (input === 1) {
+    return "0, Beep!"
+  }
 }
 
-console.log(beepBoop("0"));
+console.log(beepBoop("1"));
