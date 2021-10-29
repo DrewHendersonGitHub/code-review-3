e80434591084a80a07ada0acc33a63790757543d
Drew Henderson <drewhendersonemail@gmail.com>
2021-10-29 10:48:26 -0700

    Made it not add a comma to the end of the results

--- js/scripts.js
+++ js/scripts.js
@@ -3,35 +3,31 @@ function beepBoop(input) {
   for (let i = 0; i <= parseInt(input); i++) {
     let value = 0;
     let digits = i.toString().split("");
-    digits.forEach(function(thing) {
-      if (thing === '1') {
-        value = 1;
-      }
-    });
-    digits.forEach(function(thing) {
-      if (thing === '2') {
-        value = 2;
-      }
-    });
-    digits.forEach(function(thing) {
-      if (thing === '3') {
-        value = 3;
+    digits.forEach(function(digit) {
+      for (let j = 1; j <= 3; j++) {
+        if (digit === j.toString() && value <= j) {
+          value = j;
+        }
       }
     });
     if (value === 1) {
       result = result.concat("Beep!, ")
     }
-    if (value === 2) {
+    else if (value === 2) {
       result = result.concat("Boop!, ")
     }
-    if (value === 3) {
+    else if (value === 3) {
       result = result.concat("Won't you be my neighbor?, ")
     }
-    if (value === 0) {
+    else {
       result = result.concat(i.toString() + ", ");
     }
   }
+  result = result.split("");
+  result.pop();
+  result.pop();
+  result = result.join("");
   return result;
 }
 
-console.log(beepBoop("13"));
+console.log(beepBoop("99"));
